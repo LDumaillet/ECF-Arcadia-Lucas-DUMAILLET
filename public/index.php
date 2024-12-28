4b91adb46df82d44b1df0811d6ec3aaa09d8acc0
LDumaillet <dumaillet.lucas@gmail.com>
2024-12-28 14:37:19 +0100

    test

--- public/index.php
+++ public/index.php
@@ -1,11 +1,1 @@
-<?php
-
-require_once "../templates/index.html";
-
-include './data/login.php';
-
-if (isset($_SESSION['login'])) {
-  $pseudo = $_SESSION['login'];
-  echo "<script>alert('Bienvenue, $pseudo !');</script>";
-  unset($_SESSION['login_alert']);
-}
+<?php header('Location:/index.html ');
