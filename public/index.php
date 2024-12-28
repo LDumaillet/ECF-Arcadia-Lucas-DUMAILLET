92f10b869980731e76e3b6b9c674008eef990de5
LDumaillet <dumaillet.lucas@gmail.com>
2024-12-28 13:23:35 +0100

    test

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles/index.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
+
 <?php
 
 require_once "../templates/index.html";
